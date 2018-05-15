104d831bf8adbd317279dfa47105cdf20d0b1396
Kes Lagoni Williams <kes@prooffice.dk>
2018-05-15 22:40:24 +0200

    Added dataclasses

---
+++ P2LGamification/P2LGamification/Dataclasses/AchievementRequirement.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+namespace P2LGamification.Dataclasses
+{
+    public class AchievementRequirement
+    {
+        public int Id { get; set; }
+        public int Logins { get; set; }
+        public int Courses { get; set; }
+        public int Points { get; set; }
+
+        public AchievementRequirement()
+        {
+        }
+
+        public AchievementRequirement(DataRow dr)
+        {
+            Id = (int)dr["ID"];
+            Logins = (int)dr["Logins"];
+            Courses = (int)dr["Courses"];
+            Points = (int)dr["Point"];
+        }
+    }
+}
---
+++ P2LGamification/P2LGamification/Dataclasses/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace P2LGamification.Dataclasses
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int KundeID { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(DataRow dr)
+        {
+            Id = (int)dr["ID"];
+            Name = (string)dr["Name"];
+            KundeID = (int)dr["KundeID"];
+        }
+    }
+}
