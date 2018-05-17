0672456bdb542a6b1d9870965b5a2d1567e22c13
Kes Lagoni Williams <kes@prooffice.dk>
2018-05-17 21:42:52 +0200

    Finished ConfigureCourses.aspx, started ConfigureAchievements.aspx and updated .gitignore.

--- P2LGamification/P2LGamification/Pages/Content/ConfigureAchievements.aspx
+++ P2LGamification/P2LGamification/Pages/Content/ConfigureAchievements.aspx
@@ -1,5 +1,11 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/Master/Gamification.Master" AutoEventWireup="true" CodeBehind="ConfigureAchievements.aspx.cs" Inherits="P2LGamification.Pages.Content.ConfigureAchievements" %>
+
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div style="width: 100%; height: 60px;">
+        <telerik:RadDropDownList ID="CustomersDDL" runat="server" DataSourceID="Customers" Skin="MetroTouch" DataTextField="Name" DataValueField="ID"></telerik:RadDropDownList>
+    </div>
+    <asp:SqlDataSource ID="Customers" runat="server" ConnectionString="<%$ ConnectionStrings:DBConnectionString %>" SelectCommand="SELECT * FROM [Customer]"></asp:SqlDataSource>
 </asp:Content>
--- P2LGamification/P2LGamification/Pages/Content/ConfigureCourses.aspx.cs
+++ P2LGamification/P2LGamification/Pages/Content/ConfigureCourses.aspx.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using P2LGamification.Datahandlers;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
 
 namespace P2LGamification.Pages.Content
 {
@@ -12,6 +15,45 @@ namespace P2LGamification.Pages.Content
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void AddBtn_Click(object sender, EventArgs e)
+        {
+            if (ChbxListScoreSettings.SelectedItems.Count > 0 && PointBox.Value > 0)
+            {
+                DBHandler db = new DBHandler();
+                int percentageCompletion = Convert.ToInt32(CompletionDDL.SelectedValue);
+                int time = 0;
+                int completion = 0;
+
+                foreach (ButtonListItem item in ChbxListScoreSettings.SelectedItems)
+                {
+                    if (item.Value.Equals("0"))
+                    {
+                        time = 1;
+                    }
+
+                    if (item.Value.Equals("1"))
+                    {
+                        completion = 1;
+                    }
+                }
+
+                db.AddParm("@Name", SqlDbType.VarChar, NameBox.Text);
+                db.AddParm("@CustomerID", SqlDbType.Int, CustomersDDL.SelectedValue);
+                db.AddParm("@CompletionPercentage", SqlDbType.Int, percentageCompletion);
+                db.AddParm("@Time", SqlDbType.Bit, time);
+                db.AddParm("@Completion", SqlDbType.Bit, completion);
+                db.AddParm("@MaxScore", SqlDbType.Int, PointBox.Value);
+
+                db.ExecuteNonStp("AddCourse");
+
+                CurrentCourses.DataBind();
+            }
+            else
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('Du skal udfylde alle scoreboard indstillinger, minimum 1 af parameterne skal vælges til.');", true);
+            }
         }
     }
 }
