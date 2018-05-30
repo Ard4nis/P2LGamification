cead2243c4df8334e2fb3672158c77dbb9c64a1d
Kes Lagoni Williams <kes@prooffice.dk>
2018-05-30 16:54:53 +0200

    Updated Dataclasses and datahandlers. Start setup of simulate.aspx

---
+++ P2LGamification/P2LGamification/Datahandlers/ScoreHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace P2LGamification.Datahandlers
+{
+    public class ScoreHandler
+    {
+        public void AddScore(int percentage, int maxScore, int courseID, int userID)
+        {
+            int point = maxScore * (percentage / 100);
+            DBHandler handler = new DBHandler();
+
+            handler.AddParm("@Point", System.Data.SqlDbType.Int, point);
+            handler.AddParm("@UserID", System.Data.SqlDbType.Int, userID);
+            handler.AddParm("@CourseID", System.Data.SqlDbType.Int, courseID);
+
+            handler.ExecuteNonSql("AddScore");
+        }
+    }
+}
